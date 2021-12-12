ea0e1234b161550983cc99c469539135ef786171
arifBurakDemiray <burakdemiray09@hotmail.com>
2021-12-12 16:18:18 +0300

    feat: enable gravity

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -328,9 +328,12 @@ export class Canvas extends LitElement {
         this.raycaster.ray.origin.copy(this.controls.getObject().position);
         this.raycaster.ray.origin.y -= 10;
 
-        // const intersections = this.raycaster.intersectObjects(objects, false);
+        const intersections = this.raycaster.intersectObjects(
+          [this.camera, this.controls.getObject()],
+          false,
+        );
 
-        // const onObject = intersections.length > 0;
+        const onObject = intersections.length > 0;
 
         const delta = (time - this.prevTime) / 1000;
 
@@ -348,22 +351,16 @@ export class Canvas extends LitElement {
         if (this.moveLeft || this.moveRight)
           this.velocity.x -= this.direction.x * 400.0 * delta;
 
-        // if (onObject === true) {
-        //   this.velocity.y = Math.max(0, this.velocity.y);
-        //   this.canJump = true;
-        // }
+        if (onObject === true) {
+          this.velocity.y = Math.max(0, this.velocity.y);
+          this.canJump = true;
+        }
 
         this.controls.moveRight(-this.velocity.x * delta);
         this.controls.moveForward(-this.velocity.z * delta);
 
         this.controls.getObject().position.y += this.velocity.y * delta; // new behavior
-
-        if (this.controls.getObject().position.y < 10) {
-          this.velocity.y = 0;
-          this.controls.getObject().position.y = 10;
-
-          this.canJump = true;
-        }
+        console.log(this.controls.getObject().position.y);
         this.prevTime = time;
       }
 
